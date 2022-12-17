408c1c9cd6fd0ce234b7b21a2f7adc39a29e0550
Anuja Jayathilaka <anuja.jayathilaka@cammsgroup.com>
2022-12-17 20:37:19 +0530

    inherit base component

--- src/app/current-weather/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather/current-weather.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '@core/base/base.component';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.scss']
+  styleUrls: ['./current-weather.component.scss'],
 })
-export class CurrentWeatherComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CurrentWeatherComponent extends BaseComponent implements OnInit {
+  constructor() {
+    super();
   }
 
+  ngOnInit(): void {}
 }
--- src/app/forecast/forecast/forecast.component.ts
+++ src/app/forecast/forecast/forecast.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '@core/base/base.component';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.scss']
+  styleUrls: ['./forecast.component.scss'],
 })
-export class ForecastComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ForecastComponent extends BaseComponent implements OnInit {
+  constructor() {
+    super();
   }
 
+  ngOnInit(): void {}
 }
