0ecb149771693ae98008235512c1330f78d9d1c2
Anuja Jayathilaka <anuja.jayathilaka@cammsgroup.com>
2022-12-17 19:01:56 +0530

    Register modules in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavigationComponent } from './side-navigation/side-navigation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ForecastModule } from './forecast/forecast.module';
+import { CurrentWeatherModule } from './current-weather/current-weather.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ForecastModule,
+    CurrentWeatherModule
   ],
   providers: [],
   bootstrap: [AppComponent]
