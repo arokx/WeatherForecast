d11038372bd4fc253d062f9588cbd96b253ddce1
Anuja Jayathilaka <anuja.jayathilaka@cammsgroup.com>
2022-12-17 19:50:21 +0530

    set paths in tsconfig.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CURRENT, FORECAST } from './core/constants/app-urls.constant';
+import { CURRENT, FORECAST } from '@core/constants/app-urls.constant';
 
 const routes: Routes = [
   {
