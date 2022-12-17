0d22eb42ea3fc12d7c8151f90ff9b4fbf6d7e05e
Anuja Jayathilaka <anuja.jayathilaka@cammsgroup.com>
2022-12-17 18:56:32 +0530

    add current weather module and the component

---
+++ src/app/current-weather/current-weather.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+
+
+
+@NgModule({
+  declarations: [
+    CurrentWeatherComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CurrentWeatherModule { }
