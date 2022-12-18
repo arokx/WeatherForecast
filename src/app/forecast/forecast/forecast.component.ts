74825cfc0c99fd455b4e484af3829e9ddee24632
Anuja Jayathilaka <anuja.jayathilaka@cammsgroup.com>
2022-12-18 13:10:43 +0530

    Add forecast data from the weather api

--- src/app/forecast/forecast/forecast.component.ts
+++ src/app/forecast/forecast/forecast.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '@core/base/base.component';
+import { Forecast } from '@core/models/forecast.model';
+import { ShareDataService } from '@core/services/base/share-data.service';
+import { ForecastService } from '../services/forecast.service';
 
 @Component({
   selector: 'app-forecast',
@@ -7,9 +10,37 @@ import { BaseComponent } from '@core/base/base.component';
   styleUrls: ['./forecast.component.scss'],
 })
 export class ForecastComponent extends BaseComponent implements OnInit {
-  constructor() {
+  forecast: Forecast | undefined;
+  searchCriteria: string = '';
+
+  constructor(
+    private forecastService: ForecastService,
+    private shareDataService: ShareDataService
+  ) {
     super();
+    this.subscriptions.add(
+      this.shareDataService.getSearchCriterias().subscribe((res) => {
+        if (res) {
+          this.searchCriteria = res;
+          this.loadWeatherForecast(this.searchCriteria);   
+        }
+      })
+    );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadWeatherForecast(this.searchCriteria);
+  }
+
+  loadWeatherForecast(searchCriteria: string) {
+    this.subscriptions.add(
+      this.forecastService
+        .loadWeatherForecast(searchCriteria)
+        .subscribe((res) => {
+          if (res) {
+            this.forecast = res.forecast;
+          }
+        })
+    );
+  }
 }
---
+++ src/app/forecast/services/forecast.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApiBaseService } from '@core/services/base/api-base.service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ForecastService {
+  constructor(private apiBaseService: ApiBaseService) {}
+
+  loadWeatherForecast(searchCriteria: string): Observable<any> {
+    if (searchCriteria == '')
+      searchCriteria = environment.defaultSearchCriteria;
+    return this.apiBaseService.get<any>([this.createPath(searchCriteria)]);
+  }
+
+  createPath(searchCriteria: string): string {
+    let forecast = 'forecast.json';
+    let key = `?key=${environment.key}`;
+    let parameters = `&q=${searchCriteria}&days=10&aqi=no&alerts=no`;
+    let path = forecast + key + parameters;
+
+    return path;
+  }
+}
