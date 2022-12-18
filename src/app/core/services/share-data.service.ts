ba8af4ac8697d5f2e81b545b8a947fb6a5662ccf
Anuja Jayathilaka <anuja.jayathilaka@cammsgroup.com>
2022-12-19 01:19:38 +0530

    issue fixes

--- src/app/core/services/share-data.service.ts
+++ src/app/core/services/share-data.service.ts
@@ -5,15 +5,21 @@ import { Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class ShareDataService {
-  private searchCriterias = new Subject<string>();
+  private searchCriterias$ = new Subject<string>();
+  private searchCriteria: string = '';
 
   constructor() {}
 
   setSearchCriterias(searchCriterias: string) {
-    this.searchCriterias.next(searchCriterias);
+    this.searchCriteria = searchCriterias;
+    this.searchCriterias$.next(searchCriterias);
   }
 
   getSearchCriterias() {
-    return this.searchCriterias.asObservable();
+    return this.searchCriterias$.asObservable();
+  }
+
+  getSearchCriteriasText() {
+    return this.searchCriteria;
   }
 }
--- src/app/current-weather/services/current-weather.service.ts
+++ src/app/current-weather/services/current-weather.service.ts
@@ -10,13 +10,6 @@ export class CurrentWeatherService {
   constructor(private apiBaseService: ApiBaseService) {}
 
   loadCurrentWeather(searchCriteria: string): Observable<any> {
-    if (searchCriteria == '') {
-      if (localStorage.getItem('GeoLocation')?.toString()!) {
-        searchCriteria = localStorage.getItem('GeoLocation')?.toString()!;
-      } else {
-        searchCriteria = environment.defaultSearchCriteria;
-      }
-    }
     return this.apiBaseService.get<any>([this.createPath(searchCriteria)]);
   }
 
